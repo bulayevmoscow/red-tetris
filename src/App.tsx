3869c40c643750d4c93c4a9c8899d73e1f4a65fd
kupsyloc <alexzlow@gmail.com>
2022-08-10 22:31:00 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,14 @@
-import React, { useMemo, useState } from 'react';
-import {
-  StyledTetris,
-  StyledTetrisWrapper,
-} from './App.styles';
+import { useState } from 'react'
 
-import {
-  Stage,
-  Display,
-  Cell,
-  StartButton,
-} from './components';
 
 function App() {
-  const [dropTime, setDropTime] = useState<
-    undefined | number
-  >(undefined);
-  const [gameOver, setGameOver] = useState(true);
+  const [count, setCount] = useState(0)
 
   return (
-    <StyledTetrisWrapper role="button" tabIndex={0}>
-      <StyledTetris>
-        {gameOver ? (
-          <>
-            <Display
-              gameOver={gameOver}
-              text={'GAME OVER!'}
-            />
-            <StartButton callback={() => undefined} />
-          </>
-        ) : (
-          <>
-            <Display text={'Score: '} />
-            <Display text={'Rows: '} />
-            <Display text={'Level: '} />
-          </>
-        )}
-      </StyledTetris>
-    </StyledTetrisWrapper>
-  );
+    <div className="App">
+
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { Cell } from './Cell/Cell';
-export { Display } from './Display/Display';
-export { Stage } from './Stage/Stage';
-export { StartButton } from './StartButton/StartButton';
