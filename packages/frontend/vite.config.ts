8dd0d59b866bcf2ce02804659764ac36a6da7e41
kupsyloc <alexzlow@gmail.com>
2022-09-18 19:28:32 +0300

    added lint

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import checker from 'vite-plugin-checker';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    svgr(),
+    react(),
+    checker({
+      typescript: true,
+    }),
+    tsconfigPaths(),
+  ],
+  server: {
+    port: 3003,
+  },
+});
